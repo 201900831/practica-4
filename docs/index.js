42ac6c6af1f90f2a99a57fcaf0fc76aa2a6de078
201900831 <201900831@alu.comillas.edu>
2022-02-27 12:39:12 +0100

    pract4

--- docs/index.js
+++ docs/index.js
@@ -1,21 +1,33 @@
-    let activity= {};
+ /*   let activity= {};
     let getAct = async ()=>{
-        let response= await fetch("https://www.boredapi.com/api/activity/");
+        let response= await fetch("https://www.boredapi.com/api/activity?participants=1");
 
 
         if(response.ok){
-        activity= await response.json();
-        console.log(activity);
-        document.getElementById('setup').innerHTML = activity.setup;
-
+        act= await response.json();
+        console.log(act);
+        document.getElementById('activity').innerHTML = act.activity;
+        document.getElementById('participants').innerHTML = act.participants;
+        document.getElementById('price').innerHTML = act.price;
+        document.getElementById('type').innerHTML = act.type;
 
         }
-    }
-    getAct();
-
-    function getActivity() {
-
-
-          document.getElementById('setup').innerHTML = activity.delivery;
-
-          }
+    }*/
+let url= 'https://www.boredapi.com/api/activity?participants=';
+let = act = {}
+let getAct= async() => {
+    participants = document.querySelector('#select');
+    value=participants.value;
+    console.log(participants);
+    link=url+value;
+    console.log(link)
+    let response = await fetch(link);
+    if(response.ok){
+        act= await response.json();
+        console.log(act);
+        document.getElementById('activity').innerHTML = act.activity;
+        document.getElementById('participants').innerHTML = act.participants;
+        document.getElementById('price').innerHTML = act.price;
+        document.getElementById('type').innerHTML = act.type;
+}
+}
