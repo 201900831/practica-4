fa5e139730bfa813e523487f66154dd29b7ffc96
201900831 <201900831@alu.comillas.edu>
2022-02-27 11:44:36 +0100

    pract4

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,21 @@
-let url= 'https://www.boredapi.com/api/activity/';
-    let = res = {};
-    let getActividad= async(,)
+    let activity= {};
+    let getAct = async ()=>{
+        let response= await fetch("https://www.boredapi.com/api/activity/");
+
+
+        if(response.ok){
+        activity= await response.json();
+        console.log(activity);
+        document.getElementById('setup').innerHTML = activity.setup;
+
+
+        }
+    }
+    getAct();
+
+    function getActivity() {
+
+
+          document.getElementById('setup').innerHTML = activity.delivery;
+
+          }
